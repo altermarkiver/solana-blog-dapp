7f2f62fada3fa7321b895f5c272e97e08d49cfa6
Dovasy <dovasy0325@gmail.com>
2022-01-19 02:42:41 +0700

    test completed

---
+++ tests/blog.ts
@@ -0,0 +1,198 @@
+import * as anchor from '@project-serum/anchor';
+import { Program } from '@project-serum/anchor';
+import { Blog } from '../target/types/blog';
+const { SystemProgram } = anchor.web3;
+const assert = require("assert");
+
+
+async function createBlog(program, provider) {
+  const blogAccount = anchor.web3.Keypair.generate(); // creates random keypair
+  const genesisPostAccount = anchor.web3.Keypair.generate(); // creates random keypair
+
+  await program.rpc.initBlog({
+    accounts: {
+      authority: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+      blogAccount: blogAccount.publicKey,
+      genesisPostAccount: genesisPostAccount.publicKey,
+    },
+    signers: [blogAccount, genesisPostAccount],
+  });
+
+  const blog = await program.account.blogState.fetch(blogAccount.publicKey);
+
+  return { blog, blogAccount, genesisPostAccount };
+}
+async function createUser(program, provider) {
+  const userAccount = anchor.web3.Keypair.generate();
+
+  const name = "user name";
+  const avatar = "https://img.link";
+
+  await program.rpc.signupUser(name, avatar, {
+    accounts: {
+      authority: provider.wallet.publicKey,
+      userAccount: userAccount.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [userAccount],
+  });
+
+  const user = await program.account.userState.fetch(userAccount.publicKey);
+  return { user, userAccount, name, avatar };
+}
+async function createPost(program, provider, blogAccount, userAccount) {
+  const postAccount = anchor.web3.Keypair.generate();
+  const title = "post title";
+  const content = "post content";
+
+  await program.rpc.createPost(title, content, {
+    // pass arguments to the program
+    accounts: {
+      blogAccount: blogAccount.publicKey,
+      authority: provider.wallet.publicKey,
+      userAccount: userAccount.publicKey,
+      postAccount: postAccount.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [postAccount],
+  });
+
+  const post = await program.account.postState.fetch(postAccount.publicKey);
+  return { post, postAccount, title, content };
+}
+
+describe("blog tests", () => {
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.Blog;
+
+  it("initialize blog account", async () => {
+    const { blog, blogAccount, genesisPostAccount } = await createBlog(
+      program,
+      provider
+    );
+
+    assert.equal(
+      blog.currentPostKey.toString(),
+      genesisPostAccount.publicKey.toString()
+    );
+
+    assert.equal(
+      blog.authority.toString(),
+      provider.wallet.publicKey.toString()
+    );
+  });
+
+  it("signup a new user", async () => {
+    const { user, name, avatar } = await createUser(program, provider);
+
+    assert.equal(user.name, name);
+    assert.equal(user.avatar, avatar);
+
+    assert.equal(
+      user.authority.toString(),
+      provider.wallet.publicKey.toString()
+    );
+  });
+
+  it("creates a new post", async () => {
+    const { blog, blogAccount } = await createBlog(program, provider);
+    const { userAccount } = await createUser(program, provider);
+
+    const { title, post, content } = await createPost(
+      program,
+      provider,
+      blogAccount,
+      userAccount
+    );
+
+    assert.equal(post.title, title);
+    assert.equal(post.content, content);
+    assert.equal(post.user.toString(), userAccount.publicKey.toString());
+    assert.equal(post.prePostKey.toString(), blog.currentPostKey.toString());
+    assert.equal(
+      post.authority.toString(),
+      provider.wallet.publicKey.toString()
+    );
+  });
+
+  it("updates the post", async () => {
+    const { blog, blogAccount } = await createBlog(program, provider);
+    const { userAccount } = await createUser(program, provider);
+    const { postAccount } = await createPost(
+      program,
+      provider,
+      blogAccount,
+      userAccount
+    );
+
+    // now update the created post
+    const updateTitle = "Updated Post title";
+    const updateContent = "Updated Post content";
+    const tx = await program.rpc.updatePost(updateTitle, updateContent, {
+      accounts: {
+        authority: provider.wallet.publicKey,
+        postAccount: postAccount.publicKey,
+      },
+    });
+
+    const post = await program.account.postState.fetch(postAccount.publicKey);
+
+    assert.equal(post.title, updateTitle);
+    assert.equal(post.content, updateContent);
+    assert.equal(post.user.toString(), userAccount.publicKey.toString());
+    assert.equal(post.prePostKey.toString(), blog.currentPostKey.toString());
+    assert.equal(
+      post.authority.toString(),
+      provider.wallet.publicKey.toString()
+    );
+  });
+
+  it("deletes the post", async () => {
+    const { blogAccount } = await createBlog(program, provider);
+    const { userAccount } = await createUser(program, provider);
+    const { postAccount: postAcc1 } = await createPost(
+      program,
+      provider,
+      blogAccount,
+      userAccount
+    );
+
+    const { post: post2, postAccount: postAcc2 } = await createPost(
+      program,
+      provider,
+      blogAccount,
+      userAccount
+    );
+
+    const {
+      post: post3,
+      postAccount: postAcc3,
+      title,
+      content,
+    } = await createPost(program, provider, blogAccount, userAccount);
+
+    assert.equal(postAcc2.publicKey.toString(), post3.prePostKey.toString());
+    assert.equal(postAcc1.publicKey.toString(), post2.prePostKey.toString());
+
+    await program.rpc.deletePost({
+      accounts: {
+        authority: provider.wallet.publicKey,
+        postAccount: postAcc2.publicKey,
+        nextPostAccount: postAcc3.publicKey,
+      },
+    });
+
+    const upPost3 = await program.account.postState.fetch(postAcc3.publicKey);
+    assert.equal(postAcc1.publicKey.toString(), upPost3.prePostKey.toString());
+
+    assert.equal(upPost3.title, title);
+    assert.equal(upPost3.content, content);
+    assert.equal(upPost3.user.toString(), userAccount.publicKey.toString());
+    assert.equal(
+      upPost3.authority.toString(),
+      provider.wallet.publicKey.toString()
+    );
+  });
+});
